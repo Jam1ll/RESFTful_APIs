8ea076f3181b9321cc19b936180dec2b4ee58056
Jam1ll <jamilguzman202@gmail.com>
2026-01-10 23:38:33 -0400

    Add project files.

---
+++ core.application/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace core.application
+{
+    public static class ServiceExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+        }
+    }
+}
